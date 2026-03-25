baf614bea5f9cee42a59bce2f9297ce8d495e65e
Aditya Shrivastav <devs.aadi20@gmail.com>
2026-03-26 04:28:43 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="h-[600px] w-[350px] flex items-center justify-center text-4xl font-bold text-white bg-zinc-950">
+        Hello
+      </div>
+    </>
+  ) 
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import { crx } from '@crxjs/vite-plugin'
+import manifest from './manifest.json'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    crx({ manifest }),
+  ],
+  server: {
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      port: 5173,
+    },
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  },
+})
